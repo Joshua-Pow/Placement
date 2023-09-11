cc17f9e24a9d7b7b62b3f8cc110748d922759604
Joshua Pow <jokesjosh@gmail.com>
2023-09-11 13:15:04 -0400

    testing another deployment process for backend

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -1,11 +1,11 @@
-from flask_restx import Api
+# from flask_restx import Api
 
-from .pdf import api as pdf_api
+# from .pdf import api as pdf_api
 
-api = Api(
-    title="Placement API",
-    version="1.0",
-    description="A simple API to upload PDFs and get their shapes"
-)
+# api = Api(
+#     title="Placement API",
+#     version="1.0",
+#     description="A simple API to upload PDFs and get their shapes"
+# )
 
-api.add_namespace(pdf_api)
+# api.add_namespace(pdf_api)
--- backend/api/pdf.py
+++ backend/api/pdf.py
@@ -1,12 +1,20 @@
-from flask_restx import Namespace, Resource
+# from flask_restx import Namespace, Resource
 
-api = Namespace('pdf', description='PDF related operations')
+# api = Namespace('pdf', description='PDF related operations')
 
 
-@api.route('/')
-class Pdf(Resource):
-    def get(self):
-        return {"message": "Hello, World!"}
+# @api.route('/')
+# class Pdf(Resource):
+#     def get(self):
+#         return {"message": "Hello, World!"}
 
-    def post(self):
-        return {"message": "Hello, World!"}
+#     def post(self):
+#         return {"message": "Hello, World!"}
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "Hello World!"
