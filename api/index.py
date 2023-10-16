22014500ed7f692c4d88a041b32473e3325160ab
Joshua Pow <jokesjosh@gmail.com>
2023-10-16 19:43:28 -0400

    trying to get the prod api working on vercel

--- api/index.py
+++ api/index.py
@@ -1,10 +1,31 @@
 from flask import Flask
-from endpoints import api
 from flask_cors import CORS
+from flask_restx import Api, Namespace, Resource
 
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+api = Api(
+    title="Placement API",
+    version="1.0",
+    description="A simple API to upload PDFs and get their shapes",
+    prefix="/api",
+)
+
 api.init_app(app)
+
+pdf_namespace = Namespace("pdf", description="PDF related operations")
+api.add_namespace(pdf_namespace)
+
+
+@api.route("/")
+class Pdf(Resource):
+    def get(self):
+        return {"message": "Hello, World!"}
+
+    def post(self):
+        return {"message": "Hello, World!"}
+
 
 if __name__ == "__main__":
     app.run(debug=True)
