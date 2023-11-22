70f6e9d7a8ba65daea62bd3d172b89a30a5aa2e2
Joshua Pow <jokesjosh@gmail.com>
2023-11-22 00:28:41 -0500

    got shapes to be a bit smoother

--- next.config.js
+++ next.config.js
@@ -11,6 +11,15 @@ const nextConfig = {
       },
     ];
   },
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+    esmExternals: 'loose', // required to make Konva & react-konva work
+  },
+  webpack: (config) => {
+    config.externals = [...config.externals, { canvas: 'canvas' }]; // required to make Konva & react-konva work
+    return config;
+  },
 };
 
 module.exports = nextConfig;
--- src/components/EditSVGPage.tsx
+++ src/components/EditSVGPage.tsx
@@ -1,5 +1,3 @@
-// pages/edit-svg.tsx
-
 import React, { useEffect, useRef } from 'react';
 import paper from 'paper';
 import DOMPurify from 'dompurify';
@@ -26,22 +24,49 @@ const EditSVGPage = ({ svgString }: Props) => {
     let segment: paper.Segment | null = null;
     let path: paper.Path | null = null;
     let movePath = false;
-    paper.project.importSVG(sanitizedSvg, (svgItem) => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    paper.project.importSVG(sanitizedSvg, (svgItem: any) => {
       svgItem.fitBounds(paper.view.bounds);
       svgItem.position = paper.view.center;
-      console.log('svgItem', svgItem);
 
-      svgItem.children.forEach((child) => {
+      svgItem.children.forEach((child: paper.Path | paper.Shape) => {
         if (child instanceof paper.Path) {
-          console.log('child', child);
-          // Apply smoothing to each path
-          child.strokeColor = 'black';
-          //child.selected = true;
+          child.strokeColor = new paper.Color('black');
+          child.fillColor = new paper.Color('white');
 
-          //Only simplify the path if it has more than 20 segments
-          if (child.segments.length > 50) {
-            //Make sure to keep straight lines straight
-            child.simplify();
+          child.onMouseEnter = (event: paper.MouseEvent) => {
+            console.log('mouseEnter', event);
+            event.target.selected = true;
+          };
+          child.onMouseLeave = (event: paper.MouseEvent) => {
+            console.log('mouseLeave', event);
+            event.target.selected = false;
+          };
+
+          // Go through all points
+          // Remove the ones that are closer than 5pt to the previous point
+          // Need to take into account the shapes total size
+
+          for (let i = 0; i < child.segments.length; i++) {
+            const currentSegment = child.segments[i];
+            const previousSegment = child.segments[i - 1];
+            if (previousSegment) {
+              const distance = currentSegment.point.getDistance(
+                previousSegment.point,
+              );
+
+              if (distance < 10) {
+                currentSegment.remove();
+                i--;
+              }
+            }
+          }
+
+          //To prevent simplifying rectangles
+          if (child.segments.length > 10) {
+            //child.reduce();
+            //child.simplify(0.02);
+            //child.flatten(0.5);
             //child.smooth();
           }
         }
@@ -66,27 +91,20 @@ const EditSVGPage = ({ svgString }: Props) => {
         if (hitResult.type === 'segment') {
           segment = hitResult.segment;
         } else if (hitResult.type === 'stroke') {
-          const location = hitResult.location;
-          segment = path.insert(location.index + 1, event.point);
-          path.smooth();
+          //Use this to add points to the path when clicking on the stroke
+          // const location = hitResult.location;
+          // segment = path.insert(location.index + 1, event.point);
+          // path.smooth();
         }
       }
       movePath = hitResult.type === 'fill';
       if (movePath) paper.project.activeLayer.addChild(hitResult.item);
     };
 
-    paper.view.onMouseMove = (event: paper.ToolEvent) => {
-      paper.project.activeLayer.selected = false;
-      console.log('eventOver', event);
-      if (event.item) {
-        event.item.selected = true;
-      }
-    };
-
     paper.view.onMouseDrag = (event: paper.ToolEvent) => {
       if (segment) {
         segment.point = (segment.point as paper.Point).add(event.delta);
-        path?.smooth();
+        // path?.smooth();
       } else if (path) {
         path.position = (path.position as paper.Point).add(event.delta);
       }
